3a102cc991778b9068f5db52306b6bd995eeaf73
Jakub Jarosz <jakub@jarosz.dev>
2023-08-01 15:48:51 +0100

    Store items sent by multiple clients

--- groupthink_test.go
+++ groupthink_test.go
@@ -37,3 +37,44 @@ func TestServerStoresItemSentByClient(t *testing.T) {
 		t.Error(cmp.Diff(want, got))
 	}
 }
+
+func TestServerStoresItemsSentByMultipleClients(t *testing.T) {
+	srv := groupthink.Server{}
+	err := srv.Listen(":0")
+	if err != nil {
+		t.Fatal(err)
+	}
+	go srv.Serve()
+
+	conn1, err := net.Dial("tcp", srv.Listener.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Fprintln(conn1, "First Idea")
+
+	var dummy string
+	_, err = fmt.Fscanln(conn1, &dummy)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	conn2, err := net.Dial("tcp", srv.Listener.Addr().String())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Fprintln(conn2, "Second Idea")
+
+	_, err = fmt.Fscanln(conn2, &dummy)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	got := srv.Items()
+	want := []string{"First Idea", "Second Idea"}
+
+	if !cmp.Equal(want, got) {
+		t.Error(cmp.Diff(want, got))
+	}
+}
