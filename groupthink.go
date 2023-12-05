4278864fa1b1952acf9f2c06dcb3d402b697e9bb
Jakub Jarosz <jakub@jarosz.dev>
2023-12-05 13:23:11 +0000

    WIP - run AI client with chatgpt

--- groupthink.go
+++ groupthink.go
@@ -2,11 +2,15 @@ package groupthink
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"strings"
 	"sync"
+
+	"github.com/mr-joshcrane/oracle"
 )
 
 type Server struct {
@@ -113,4 +117,42 @@ func (c *Client) AddItem(item string) error {
 		c.Items = append(c.Items, item)
 	}
 	return nil
+}
+
+// RunAIClient creates a new GroupThink client with ChatGPT support.
+func RunAIClient() {
+	c, err := NewClient(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "server uri required first argument")
+		os.Exit(1)
+	}
+	token := os.Getenv("OPENAI_API_KEY")
+	if token == "" {
+		fmt.Fprintln(os.Stderr, "OpenAI API token not exported")
+		os.Exit(1)
+	}
+	o := oracle.WithGPT35Turbo()(oracle.NewOracle(token))
+	// o.SetPurpose("You generate a single creative and tangential suggestion in a brainstorming session.")
+	// o.GiveExample("Understand how OAuth works", "Create a CLI application that utilizes device flow")
+	o.SetPurpose("Give similar suggestions.")
+	o.GiveExample("rugby", "football")
+
+	err = c.AddItem("")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	for {
+		out := c.Items
+		query := strings.Join(out, "\n")
+		fmt.Println("ITEMS>>>", query)
+		answer, err := o.Ask(context.Background(), query)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		c.AddItem(strings.Split(answer, "\n")[0])
+		fmt.Println("PRINT>>", strings.Split(answer, "\n")[0])
+	}
 }
